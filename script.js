1980aedaad524474009c650efc6b4ef5dd2b42f0
scridonlucas <lukas_scridon@yahoo.com>
2022-10-10 01:04:25 +0300

    Adaugare functii

--- script.js
+++ script.js
@@ -1,12 +1,44 @@
 function getComputerChoice() {
     let number = Math.floor(Math.random() * 3);
     if(number == 0) {
-        return "Rock";
+        return "rock";
     }
     else if(number == 1) {
-        return "Scissors"
+        return "scissors"
     }
     else {
-        return "Paper";
+        return "paper";
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    let playerSelection1 = playerSelection.toLowerCase();
+    if(playerSelection === computerSelection) {
+        return "Draw!"
+    } else if (playerSelection === "rock") {
+        if(computerSelection === "paper") {
+            return "You Lose! Paper beats Rock"
+        } else if(computerSelection === "scissors") {
+            return "You Win! Rock beats Scissors"
+        }
+    } else if (playerSelection === "paper") {
+        if(computerSelection === "scissors") {
+            return "You Lose! Scissors beats Papaer"
+        } else if(computerSelection === "rock") {
+            return "You Won! Scissors beats Rock"
+        }
+    } else if (playerSelection === "scissors") {
+        if(computerSelection === "rock") {
+            return "You lose! Rock beats Scissors"
+        } else if(computerSelection === "Paper") {
+            return "You won! Scissors beats Paper"
+        }
+    }
+}
+
+function game() {
+    for(let i = 0; i < 5; i++) {
+        let input = prompt("Rock, Paper, Scissors?");
+        playRound(input, getComputerChoice());
     }
 }
